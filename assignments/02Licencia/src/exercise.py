1541ac410e99873e10f187aab920912e5402895f
Francisco Javier Rivera Ruiz <A01732785@itesm.mx>
2021-08-26 10:39:28 -0500

    Tareaterminada

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,21 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
-    #Aquí empieza tu programa...
+
+    if edad >= 18:
+        documentos = str(input("¿Tienes identificación oficial? (s/n): "))
+        if documentos == "s":
+            print("Trámite de licencia concedido")
+        elif documentos == "n":
+            print("No cumples requisitos")
+        else: 
+            print("Respuesta incorrecta")
+    elif edad > 0 and edad < 18:
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
+        
+    
 
 if __name__ == '__main__':
     main()
